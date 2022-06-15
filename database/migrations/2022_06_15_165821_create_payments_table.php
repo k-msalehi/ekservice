c9a581e6a9eb43342b7ab9546f40744a160e1313
MohammadSalehi <mds1121@yahoo.com>
2022-06-15 17:53:23 +0430

    updates

---
+++ database/migrations/2022_06_15_165821_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders');
+            $table->foreignId('user_id')->constrained('users');
+            $table->unsignedInteger('amount');
+            $table->string('bank_ref_id'); // bank reference id for payment
+            $table->string('bank_sale_order_id'); // bank sale order id (for bank payment)
+            $table->string('bank_sale_refrence_id'); // bank sale reference id (transaction id)
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
